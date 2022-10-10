b39e059a9db5b00a30d11dabdde0b44a0e88b7a2
GreenallWang <112460654+GreenallWang@users.noreply.github.com>
2022-10-10 20:41:42 +0800

    Add files via upload

---
+++ if2.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <math.h>
+int main ()
+{
+	double a,b,c,area,perimeter,s;
+	scanf("%lf%lf%lf",&a,&b,&c);
+	if(a+b>c&&a+c>b&&b+c>a)
+		{
+		s=(a+b+c)/2.0;
+		area=sqrt(s*(s-a)*(s-b)*(s-c));
+		perimeter=a+b+c;
+		printf("%.2lf,%.2lf",area,perimeter);
+		}
+	else
+		printf("Error!");
+	return 0;	
+}
+
